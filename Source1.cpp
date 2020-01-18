2b4670ba8c283c6ed170f82a4d011513b0cf96de
04603aslan <baris_karapelit_298@hotmail.com>
2020-01-18 03:48:54 +0200

    Deneme

---
+++ Source1.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string header = "****************Calculater*******************";
+	string a, b;
+	cout << header<<endl;
+	cout << "Enter the a Number :"<<endl;
+	cin >> a; 
+	while(a==char)
+	{
+		cin>>a;
+	}
+	cout << "Enter the a Number :"<<endl;
+	cin >> b;
+	cout << "Do you Operating choice : + / * -" << endl;
+	char  choice;
+	cin >> choice;
+	switch (choice)
+	{
+	case '+':
+		cout<< a + b;
+		break;
+	case '-':
+		cout<< a - b;
+		break;
+	case '/':
+		cout << a / b;
+		break;
+	case '*':
+		cout << a * b;
+		break;
+	default:
+		cout << "Please Enter  the character";
+		break;
+	}
+
+
+	
+
+}
